ed0bb8508f6cfb4cc765f4d8c9ef890c736302ab
g-veronika <g.veronika2021@gmail.com>
2021-11-24 16:32:18 +0100

    initial commit

---
+++ my-calendar-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import {DatePicker} from './Components/DatePicker'
+
+function App() {
+  return (
+    <DatePicker />
+  );
+}
+
+export default App;
---
+++ my-calendar-app/src/Components/DatePicker.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const DatePicker: React.FC<{}> = ({}) => {
+    return (
+
+        <div className="date-picker-container">
+            <div className="background-container"></div>
+        </div>
+
+    )
+}
